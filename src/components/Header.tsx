9451a8c9d601dfe1030651655839be6f237c4251
Victor Lucas A. de Oliveira <59880314+victorlucass@users.noreply.github.com>
2022-10-08 03:13:58 -0400

    feat: effect smooth scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,34 @@
 import { HeaderContainer } from "./Header.styled";
 
 export function Header() {
+  function scrollToSection(event: any) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+
   return (
     <HeaderContainer>
       <h1>Victor Lucas</h1>
-      <nav>
+      <nav className="js-navigation">
         <ul>
           <li>
-            <a href="#experience">Experiência</a>
+            <a href="#experience" onClick={scrollToSection}>
+              Experiência
+            </a>
           </li>
           <li>
-            <a href="#formation">Formação</a>
+            <a href="#formation" onClick={scrollToSection}>
+              Formação
+            </a>
           </li>
           <li>
-            <a href="#contact">Contato</a>
+            <a href="#contact" onClick={scrollToSection}>
+              Contato
+            </a>
           </li>
         </ul>
       </nav>
